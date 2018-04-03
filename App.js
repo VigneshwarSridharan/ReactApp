bbee831cafe0bea3b33f6d2bda96b3f30fe8d3f8
vigneshwar <s.vigneshwar@wintlt.com>
2018-04-03 11:13:23 +0530

    02-04-2018

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { StackNavigator, DrawerNavigator } from 'react-navigation';
 
 import HomeScreen from './components/home-screen';
 import FlightResult from './components/flight-result';
 import ItinDetails from './components/itin-details';
 import PassengerDetails from './components/passenger-details';
+import BookingPage from './components/booking-page';
 
-const RootStack = StackNavigator(
+const StackScreens = StackNavigator(
   {
     Home: {
       screen: HomeScreen,
@@ -20,11 +21,21 @@ const RootStack = StackNavigator(
     },
     PassengerDetailsScreen: {
       screen: PassengerDetails,
+    },
+    BookingPageScreen: {
+      screen: BookingPage,
     }
   },
   {
     initialRouteName: 'Home',
     navigationOptions: {
+      headerRight: (
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 15 }}>
+          <Image source={require('./assets/images/ca.png')} style={{ height: 32, width: 32, marginRight: 10 }} />
+          <Text style={{ color: '#ffffff', fontSize: 20 }}>CAD</Text>
+        </View>
+
+      ),
       headerStyle: {
         backgroundColor: '#e67e22',
       },
@@ -36,8 +47,30 @@ const RootStack = StackNavigator(
   }
 );
 
+const RootStack = DrawerNavigator({
+  Flights: {
+    screen: StackScreens,
+    navigationOptions: {
+      title: 'Flights'
+    }
+  }
+}, {
+  contentOptions: {
+    activeTintColor : '#e67e22',
+    labelStyle : {
+      fontSize: 18
+    }
+  }
+});
+
 export default class App extends Component {
   render() {
+    var navigationView = (
+      <View style={{flex: 1, backgroundColor: '#fff'}}>
+        <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
+      </View>
+    );
+  
     return <RootStack />
   }
 }
